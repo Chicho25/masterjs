10a28d7e7acd351b8b7c56f331102f1547ebdd8a
Pedro Arrieta <37045854+Chicho25@users.noreply.github.com>
2019-03-02 18:29:41 -0500

    arrays

---
+++ js/25-arrays.js
@@ -0,0 +1,9 @@
+'use strict'
+// arrays
+
+var nombre = "tayron";
+var nombres = ["Chicho", "Blaster", "Tayron"];
+//var array_php = ["nombre1" => "Pedro",
+  //               "nombre2" => "David"];
+
+//console.log(array_php["nombre2"]);
